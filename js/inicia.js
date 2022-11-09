43592b97b29a1deaee9e8c9b4e732fc8c0eb3f2f
cmaroto307 <109908350+cmaroto307@users.noreply.github.com>
2022-11-09 19:17:42 +0100

    Add files via upload

--- js/inicia.js
+++ js/inicia.js
@@ -1,25 +1,28 @@
 import anime from "./anime.js";
 
 let NUMCUADRADOS = 20;
-let MAXDESPLAZAMIENTO = 1000;
+let RANGO1 = 300;
+let RANGO2 = 400;
+let RANGO3 = 1000;
 
 function * generador() {
     while(true){
-        yield Math.floor(Math.random() * MAXDESPLAZAMIENTO);
+        yield Math.floor(Math.random() * RANGO1);
+        yield Math.floor(Math.random() * RANGO2);
+        yield Math.floor(Math.random() * RANGO3);
     }
 };
 
 const gen = generador();
+let elem = document.getElementById("idContenedor");
+while(elem.hasChildNodes()) elem.removeChild(elem.firstChild);
+for (let i=0; i< NUMCUADRADOS; i++) {
+    let nodo = document.createElement("div");
+    nodo.className = "cuadrado"
+    elem.appendChild(nodo);
+}
   
 function moverCuadrado(){
-
-    let elem = document.getElementById("idContenedor");
-    while(elem.hasChildNodes()) elem.removeChild(elem.firstChild);
-    for (let i=0; i< NUMCUADRADOS; i++) {
-        let nodo = document.createElement("div");
-        nodo.className = "cuadrado"
-        elem.appendChild(nodo);
-    }
     
     anime.timeline({
         targets: '.cuadrado',
@@ -27,7 +30,6 @@ function moverCuadrado(){
         duration: 2000,
         endDelay: 400
     }).add({
-        targets: '.cuadrado',
         translateX: function() {
             return gen.next().value;
         }
